a2f4f55090335d15ccec50b3abe8901d43db5cce
Sabina <124915433+Sabinashamurzaeva@users.noreply.github.com>
2025-06-03 09:35:51 +0300

    matrix factorization and  colloborative filtration

--- recommender/models.py
+++ recommender/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
-class MovieDescriptions(models.Model):
-    movie_id = models.CharField(max_length=16)
-    imdb_id = models.CharField(max_length=16)
-    title = models.CharField(max_length=512)
-    description = models.CharField(max_length=1024)
-    genres = models.CharField(max_length=512, default='')
-    lda_vector = models.CharField(max_length=56, null=True)
-    sim_list = models.CharField(max_length=512, default='')
 
-    class Meta:
-        db_table = 'movie_description'
+class MovieDescriptions(models.Model):
+    movie_id = models.CharField(max_length=16, primary_key=True)
+    imdb_id = models.CharField(max_length=16, unique=True)
+    title = models.CharField(max_length=512)
+    description = models.TextField()  # Изменил на TextField для больших описаний
+    genres = models.CharField(max_length=512)
+    year = models.IntegerField(null=True)  # Добавлен год выпуска
+    rating = models.FloatField(null=True)  # Добавлен рейтинг
+    lda_vector = models.JSONField(null=True)  # Более современный вариант
+    sim_list = models.JSONField(default=list)  # Для хранения списков
+    
+class Meta:
+    db_table = 'movie_description'
+    indexes = [
+        models.Index(fields=['title']),
+        models.Index(fields=['genres']),
+    ]
 
     def __str__(self):
-        return "{}: {}".format(self.imdb_id, self.title)
+        return f"{self.title} ({self.year})"
 
 
 class LdaSimilarity(models.Model):
